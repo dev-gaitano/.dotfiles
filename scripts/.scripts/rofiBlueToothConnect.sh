78ee8809430c5cc6768250bdca197b982996c680
gaitano <dev.gaitano@gmail.com>
2025-11-30 15:10:24 +0300

    refactor: created a connect_device function

--- scripts/.scripts/rofiBlueToothConnect.sh
+++ scripts/.scripts/rofiBlueToothConnect.sh
@@ -1,46 +1,32 @@
 #!/usr/bin/env sh
 
-chooseBluetoothDevice() {
+connect_device() {
+	device_mac="$1"
+	device_name="$2"
+
+	bluetoothctl power on
+	bluetoothctl disconnect
+
+	output=$(bluetoothctl connect "$device_mac" 2>&1)
+
+	if printf "%s" "$output" | grep -qi "Connection successful"; then
+	    notify-send -u low "Bluetooth connected to $device_name"
+	else
+	    notify-send -u critical "$device_name connection failed"
+	fi
+}
+
+choose_bluetooth_device() {
 	choice=$(printf "JBL\nRS288R\nGB03\noff" | rofi -dmenu -i -p "Connect Device" -theme-str 'window {width: 16%; height: 21%;}' )
 	case "$choice" in
-		JBL) echo 1
-			bluetoothctl power on
-			bluetoothctl disconnect
-
-			output=$(bluetoothctl connect D8:AA:59:C3:85:CD 2>&1)
-
-			echo "$output" | grep -qi "Connection successful"
-			if [ $? -eq 0 ]; then
-			    notify-send -u low "Bluetooth connected to JBL"
-			else
-			    notify-send -u critical "JBL connection failed"
-			fi
+		JBL)
+			connect_device "D8:AA:59:C3:85:CD" "JBL"
 		;;
 		RS288R)
-			bluetoothctl power on
-			bluetoothctl disconnect
-
-			output=$(bluetoothctl connect 55:D2:9E:EC:AD:C2 2>&1)
-
-			echo "$output" | grep -qi "Connection successful"
-			if [ $? -eq 0 ]; then
-				notify-send -u low "Bluetooth Connected to RS288R"
-			else
-				notify-send -u critical "RS288R connection failed"
-			fi
+			connect_device "55:D2:9E:EC:AD:C2" "RS288R"
 		;;
 		GB03)
-			bluetoothctl power on
-			bluetoothctl disconnect
-
-			output=$(bluetoothctl connect 61:81:FF:D0:77:93 2>&1)
-
-			echo "$output" | grep -qi "Connection successful"
-			if [ $? -eq 0 ]; then
-				notify-send -u low "Bluetooth Connected to GB03"
-			else
-				notify-send -u critical "GB03 connection failed"
-			fi
+			connect_device "61:81:FF:D0:77:93" "GB03"
 		;;
 		off)
 			bluetoothctl power off
@@ -51,4 +37,4 @@ chooseBluetoothDevice() {
 	
 }
 
-chooseBluetoothDevice
+choose_bluetooth_device

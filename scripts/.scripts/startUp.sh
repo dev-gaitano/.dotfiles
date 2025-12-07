8c60265cc9ad594b56b5f8c10eb15e9b372b3ed7
gaitano <dev.gaitano@gmail.com>
2025-12-07 20:07:40 +0300

    refactor(scripts): reorganized session management functions for clarity and maintainability

--- scripts/.scripts/startUp.sh
+++ scripts/.scripts/startUp.sh
@@ -2,34 +2,66 @@
 
 SESSION=1914
 
-echo "Creating a new tmux session \"$SESSION\"..."
-echo " "
-
-sleep 2
-
-if tmux has-session -t "$SESSION" 2>/dev/null; then
-	echo "Session \"$SESSION\" already exists, attaching..."
+createSession1914() {
+	echo "Creating a new tmux session \"$SESSION\"..."
+	echo " "
 	sleep 2
 
+	tmux new-session -d -s "$SESSION" -n main
+	tmux send-keys -t "$SESSION:1" "clear" C-m
+	tmux send-keys -t "$SESSION:1" "~/.scripts/intro.sh" C-m
+	tmux new-window -t "$SESSION:2" -n ranger
+	tmux send-keys -t "$SESSION:2" "ranger" C-m
+	tmux new-window -t "$SESSION:3" -n ai
+	tmux send-keys -t "$SESSION:3" "gemini" C-m
+	tmux new-window -t "$SESSION:4" -n obsidian
+	tmux send-keys -t "$SESSION:4" "obsdn" C-m
+	tmux send-keys -t "$SESSION:4" "nvim ." C-m
+	tmux new-window -t "$SESSION:5" -n stats
+	tmux send-keys -t "$SESSION:5" "btop" C-m
+
+	notify-send -u low "Attached to the tmux session $SESSION."
+	tmux a -t "$SESSION":1
+}
+
+attachToExistingSession() {
+	notify-send -u low "Attached to the tmux session $SESSION."
 	tmux a -t "$SESSION"
 	exit 0
+}
+
+createNewSession() {
+	read -p "Directory (absolute) path: " SESSIONDIR # TODO: Modify so that it doesn't depend on Absolute
+	read -p "Session Name: " SESSION
+	echo " "
+
+	echo "Changing Directory..."
+	cd "$SESSIONDIR"
+	echo "Creating a new tmux session \"$SESSION\"..."
+	echo " "
+	sleep 2
+	~/.scripts/tmuxCreate.sh "$SESSION" dev
+
+	exit 0
+}
+
+if tmux has-session -t "$SESSION" 2>/dev/null; then
+	echo "Session \"$SESSION\" already exists"
+	echo " "
+	echo "Do you want to:"
+	echo " 1. Attach to session"
+	echo " 2. Create New Session"
+	echo " 3. Don't Attach nor Create"
+	read -p "(1) (2) (3) - default is (1): " answer
+	echo " "
+
+	if [ "$answer" -eq 1 ]; then
+		attachToExistingSession
+	elif [ "$answer" -eq 2 ]; then
+		createNewSession
+	else
+		attachToExistingSession
+	fi
+else
+	createSession1914
 fi
-
-tmux new-session -d -s "$SESSION" -n main
-tmux send-keys -t "$SESSION:1" "clear" C-m
-tmux send-keys -t "$SESSION:1" "~/.scripts/intro.sh" C-m
-tmux new-window -t "$SESSION:2" -n ranger
-tmux send-keys -t "$SESSION:2" "ranger" C-m
-tmux new-window -t "$SESSION:3" -n ai
-tmux send-keys -t "$SESSION:3" "gemini" C-m
-tmux new-window -t "$SESSION:4" -n obsidian
-tmux send-keys -t "$SESSION:4" "obsdn" C-m
-tmux send-keys -t "$SESSION:4" "nvim ." C-m
-tmux new-window -t "$SESSION:5" -n stats
-tmux send-keys -t "$SESSION:5" "btop" C-m
-
-tmux a -t "$SESSION":1
-
-
-echo "attached to the tmux session $SESSION."
-echo " "
